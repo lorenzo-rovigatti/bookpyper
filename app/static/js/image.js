d5e8e38e6f3d0dcbbeed8d29fcc205d5efc864b1
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2021-01-10 15:51:46 +0100

    Add the possibility of selecting, moving and resizing rectangles

--- app/static/js/image.js
+++ app/static/js/image.js
@@ -3,14 +3,18 @@ var image_layer;
 var shape_layer;
 var mode;
 var rubber_rect;
+var image;
 var image_url;
 var image_ratio;
+var selected_rect;
+var rect_transformer;
 
 const mode_enum = Object.freeze({
 	"nothing": 1,
 	"add": 2,
 	"remove": 3,
-	"drawing": 4
+	"drawing": 4,
+	"select": 5,
 });
 
 window.onload = function(event) {
@@ -29,15 +33,17 @@ window.onload = function(event) {
 	shape_layer = new Konva.Layer();
 
 	rubber_rect = new Konva.Rect({
-		x: 0,
-		y: 0,
-		width: 0,
-		height: 0,
-		stroke: 'red',
-		strokeWidth: 2,
+		fill: 'rgba(0,255,0,0.5)',
 		listening: false
 	});
 	shape_layer.add(rubber_rect);
+
+	rect_transformer = new Konva.Transformer({
+		rotateEnabled: false,
+		keepRatio: false,
+		enabledAnchors: ['top-left', 'top-right', 'bottom-left', 'bottom-right']
+	});
+	shape_layer.add(rect_transformer);
 
 	stage.add(image_layer);
 	stage.add(shape_layer);
@@ -51,14 +57,17 @@ window.onload = function(event) {
 	//document.querySelector("#file_input").addEventListener("change", load_image);
 
 	document.querySelector("#add").addEventListener("click", function(e) {
-		mode = mode_enum.add;
+		update_mode(mode_enum.add);
 	});
 	document.querySelector("#remove").addEventListener("click", function(e) {
-		mode = mode_enum.remove;
+		update_mode(mode_enum.remove);
+	});
+	document.querySelector("#select").addEventListener("click", function(e) {
+		update_mode(mode_enum.select);
 	});
 	document.querySelector("#find").addEventListener("click", function(e) {
 		show_loading();
-		
+
 		var json_body = {
 			url: image_url
 		};
@@ -71,45 +80,99 @@ window.onload = function(event) {
 				'Content-Type': 'application/json'
 			},
 		})
-		.then(function(response) {
-			return response.json();
-		})
-		.then(function(data) {
-			for (rect_id in data.rectangles) {
-				rect = data.rectangles[rect_id];
-				// scale the rectangle
-				rect.x /= image_ratio;
-				rect.y /= image_ratio;
-				rect.width /= image_ratio;
-				rect.height /= image_ratio;
-				add_rectangle(rect.x, rect.y, rect.width, rect.height);
-			}
-			
-			hide_loading();
-		});
+			.then(function(response) {
+				return response.json();
+			})
+			.then(function(data) {
+				for (rect_id in data.rectangles) {
+					rect = data.rectangles[rect_id];
+					// scale the rectangle
+					rect.x /= image_ratio;
+					rect.y /= image_ratio;
+					rect.width /= image_ratio;
+					rect.height /= image_ratio;
+					add_rectangle(rect.x, rect.y, rect.width, rect.height);
+				}
+
+				hide_loading();
+			});
 	});
 
-	stage.on('mousedown', function(e) {
+	stage.on('mousedown touchstart', function(e) {
 		if (mode == mode_enum.add) {
-			mode = mode_enum.drawing;
+			update_mode(mode_enum.drawing);
 			start_drawing({ x: e.evt.layerX, y: e.evt.layerY });
 		}
 	});
 
-	stage.on('mousemove', function(e) {
+	stage.on('mousemove touchmove', function(e) {
 		if (mode == mode_enum.drawing) {
 			update_drawing({ x: e.evt.layerX, y: e.evt.layerY });
 		}
 	});
 
 	// here we create the new rect using the location and dimensions of the drawing rect.
-	stage.on('mouseup', function(e) {
+	stage.on('mouseup touchend', function(e) {
 		if (mode == mode_enum.drawing) {
 			reset_mode();
 			rubber_rect.visible(false);
 			add_rectangle(rubber_rect.x(), rubber_rect.y(), rubber_rect.width(), rubber_rect.height());
 		}
 	})
+
+	stage.on('click tap', function(e) {
+
+		// remove all selections
+		if (e.target === image) {
+			set_as_selected([]);
+			shape_layer.draw();
+			return;
+		}
+
+		// do nothing if the user has not clicked on a rectangle
+		if (!e.target.hasName('rect')) {
+			return;
+		}
+
+		// do we pressed shift or ctrl?
+		const meta_pressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
+		const is_selected = rect_transformer.nodes().indexOf(e.target) >= 0;
+
+		if (!meta_pressed && !is_selected) {
+			// if no key pressed and the node is not selected
+			// select just one
+			set_as_selected([e.target]);
+		} 
+		else if (meta_pressed && is_selected) {
+			// if we pressed keys and node was selected
+			// we need to remove it from selection:
+			const nodes = rect_transformer.nodes().slice(); // use slice to have new copy of array
+			// remove node from array
+			nodes.splice(nodes.indexOf(e.target), 1);
+			set_as_selected(nodes);
+		} 
+		else if (meta_pressed && !is_selected) {
+			// add the node into selection
+			const nodes = rect_transformer.nodes().concat([e.target]);
+			set_as_selected(nodes);
+		}
+		shape_layer.draw();
+	});
+}
+
+function set_as_selected(nodes) {
+	// we first deselect all the nodes currently selected
+	rect_transformer.nodes().forEach(elem => {
+		elem.draggable(false);
+		elem.fill(null);
+	});
+	
+	// and then add the new ones	
+	nodes.forEach(elem => {
+		elem.draggable(true);
+		elem.fill('rgba(255,0,0,0.5)');
+	});
+	rect_transformer.nodes(nodes);
 }
 
 function add_rectangle(x, y, width, height) {
@@ -118,18 +181,24 @@ function add_rectangle(x, y, width, height) {
 		y: y,
 		width: width,
 		height: height,
-		stroke: 'black',
+		stroke: 'red',
 		strokeWidth: 2,
+		strokeScaleEnabled: false,
+		name: 'rect'
 	})
 	new_rect.on("click", function(e) {
 		if (mode == mode_enum.remove) {
 			reset_mode();
 			this.destroy();
-			shape_layer.draw();
+			shape_layer.batchDraw();
 		}
 	});
 	shape_layer.add(new_rect);
 	shape_layer.draw();
+}
+
+function update_mode(new_mode) {
+	mode = new_mode;
 }
 
 function reset_mode() {
@@ -168,7 +237,7 @@ function upload_image() {
 					image_ratio = (img_width > img_height ? (img_width / max) : (img_height / max))
 
 					// now load the Konva image
-					var my_img = new Konva.Image({
+					image = new Konva.Image({
 						image: img,
 						x: 0,
 						y: 0,
@@ -177,7 +246,7 @@ function upload_image() {
 					});
 
 					image_layer.clear();
-					image_layer.add(my_img);
+					image_layer.add(image);
 					image_layer.draw();
 				}
 			}
@@ -200,7 +269,7 @@ function update_drawing(pos) {
 	rubber_rect.height(rect_pos.y2 - rect_pos.y1);
 	rubber_rect.visible(true);
 
-	shape_layer.draw(); // redraw any changes.
+	shape_layer.batchDraw();
 }
 
 
@@ -225,7 +294,7 @@ function show_loading() {
 	document.querySelectorAll("button, input").forEach(elem => {
 		elem.disabled = true;
 	});
-	
+
 	loader.classList.add("display");
 	// to stop loading after some time
 	setTimeout(() => {
@@ -236,7 +305,7 @@ function show_loading() {
 // hide the loading animation
 function hide_loading() {
 	loader.classList.remove("display");
-	
+
 	document.querySelectorAll("button, input").forEach(elem => {
 		elem.disabled = false;
 	});
